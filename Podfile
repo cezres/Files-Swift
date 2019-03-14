4b511b191fa652dd55e27d6e00cb23cc237e659c
cezres <cezr@sina.com>
2019-03-14 16:44:34 +0800

    Setup project

---
+++ Podfile
@@ -0,0 +1,10 @@
+inhibit_all_warnings!
+platform :ios, '12.0'
+use_frameworks!
+
+
+target 'files' do
+    pod 'CocoaHTTPServer', git: "https://github.com/cezres/CocoaHTTPServer.git", commit: "acc036960f14ae1e5f09eb80a95a91489ad52f7a"
+    pod 'SnapKit'
+end
+
---
+++ files/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  files
+//
+//  Created by 翟泉 on 2019/3/14.
+//  Copyright © 2019 cezres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
