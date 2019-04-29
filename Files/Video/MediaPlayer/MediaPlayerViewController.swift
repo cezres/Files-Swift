0a28fc3dd230bd4b6102dd6a54ad6502346ae391
cezres <cezr@sina.com>
2019-04-29 11:46:27 +0800

    feat(vidoe): Add title in player

--- Files/Video/MediaPlayer/MediaPlayerViewController.swift
+++ Files/Video/MediaPlayer/MediaPlayerViewController.swift
@@ -23,15 +23,32 @@ class MediaPlayerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
 
         // Do any additional setup after loading the view.
         view.addSubview(playerView)
+
         topView.contentView.addSubview(backButton)
+        topView.contentView.addSubview(titleLabel)
         view.addSubview(topView)
 
+        backButton.snp.makeConstraints { (maker) in
+            maker.width.equalTo(30)
+            maker.height.equalTo(30)
+            maker.left.equalTo(15)
+            maker.centerY.equalTo(topView.contentView)
+        }
+        titleLabel.snp.makeConstraints { (maker) in
+            maker.left.equalTo(backButton.snp.right).offset(15)
+            maker.right.equalTo(-15)
+            maker.top.equalTo(0)
+            maker.bottom.equalTo(0)
+        }
+
         playerView.registerControl(MediaPlayerControlView())
+
         playerView.play(url)
+        titleLabel.text = url.lastPathComponent
     }
 
     override func viewWillLayoutSubviews() {
@@ -39,7 +56,6 @@ class MediaPlayerViewController: UIViewController {
         if view.size != playerView.size {
             playerView.frame = CGRect(x: 0, y: 0, width: view.width, height: view.height)
             topView.frame = CGRect(x: 0, y: 0, width: view.width, height: 44)
-            backButton.frame = CGRect(x: 15, y: (topView.height - 30) / 2, width: 30, height: 30)
         }
     }
 
@@ -63,5 +79,11 @@ class MediaPlayerViewController: UIViewController {
         button.setImage(UIImage(named: "icon_media_player_back")!, for: .normal)
         button.addTarget(self, action: #selector(back), for: .touchUpInside)
         return button
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        return label
     }()
 }
