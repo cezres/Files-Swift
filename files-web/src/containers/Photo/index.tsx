975c110afc6bd0092bd864c59005834010bfb9ae
cezres <cezres@163.com>
2019-09-16 23:46:45 +0800

    feat(web): add photo page

---
+++ files-web/src/containers/Photo/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getUrlParams } from '../../utils/util'
+import { baseURL } from '../../services/fetcher'
+
+export default (props: any) => {
+  let path: string | undefined
+  const params = getUrlParams(props.location)
+  if (params.path) {
+    path = params.path
+  } else {
+    path = ''
+  }
+  console.log(`path = ${path}`);
+
+  return (
+    <img src={`${baseURL}/document/data?path=${path}`}></img>
+  )
+}
